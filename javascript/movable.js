b90f32762ad03fec7af377497fd43805e8a68a8d
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-09-23 14:39:23 +0200

    Targets management affected to controlable objects

--- javascript/movable.js
+++ javascript/movable.js
@@ -28,14 +28,6 @@ var Movable=new Class({
 		this.prevX=this.x;
 		this.prevY=this.y;
 		this.prevA=this.a;
-		// Computing targets
-		if(this.life)
-			this.target();
-		else
-			{
-			this.way=0;
-			this.direction=0;
-			}
 		// Rotating
 		if(this.direction!=0)
 			{
@@ -87,42 +79,6 @@ var Movable=new Class({
 			this.way=0;
 		else
 			this.way=way;
-		},
-	// Targets management
-	setTargets : function() {
-		this.targets=new Array();
-		for(var i=0, j=arguments.length; i<j; i++)
-			this.targets.push(arguments[i]);
-		},
-	addTarget : function(target) {
-		this.targets.push(target);
-		},
-	target : function(distanceMin) {
-		if(this.targets.length)
-			{
-			var a=Math.round((((2*Math.PI)+Math.atan2(this.targets[0].y-this.y, this.targets[0].x-this.x))%(2*Math.PI))/(2*Math.PI)*16)%16;
-			// Adjusting the direction to atteign it (needs improvement)
-			if(a-this.a<0)
-				this.direction=-1;
-			else if(a-this.a>0)
-				this.direction=1;
-			else
-				this.direction=0;
-			// Measuring the distance to the target
-			if(Math.sqrt(Math.pow(this.x-this.targets[0].x,2) + Math.pow(this.y-this.targets[0].y,2))<(distanceMin?distanceMin:this.game.tileSize))
-				{
-				if(this.direction==0)
-					this.targets.splice(0,1);
-				this.way=0;
-				}
-			// Finding the angle
-			else
-				{
-				this.way=1;
-				}
-			return a;
-			}
-		return -1;
 		},
 	// Hits management
 	hits : function() {
